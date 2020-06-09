07bd55959e47ab9dc8da8567c5f42b9cda7af1d8
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2020-06-09 01:18:47 -0600

    Add camera controller

---
+++ Chess/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float horizontalSpeed = 2.0f;
+
+    //TODO Get peice selection working
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+
+        if (Input.GetMouseButton(1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            transform.Rotate(0, h, 0);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+}
